a36f0056f705c131bba9e76abc3bec40076fca2c
Ignacio Garcia <69655673+ilqcho@users.noreply.github.com>
2021-06-07 10:11:25 -0300

    Delete pygrupal directory

--- pygrupal/src/personas/Imagen.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Imagen(props){
-    return(
-        <div>
-            <img src={props.url} class="imagen-personas" alt="img" />
-        </div>
-    )
-}
